9d2861dc5286435524fba45871038daa3c6328bf
Shay Hill <shay_public@hotmail.com>
2023-08-09 09:05:26 -0500

    refactor: remove reduntant time distortions
    Removed the power time distortions and made push_i symmetrical with push_o.
    
    BREAKING CHANGE: push_i time distortion gives different result than previous. `power` time distortion removed.

--- src/posterize/fat_tonys.py
+++ src/posterize/fat_tonys.py
@@ -15,6 +15,7 @@ import posterize.time_distortions as dist
 
 from posterize.main import posterize_with_outline
 
+
 def _hex_interp(hex_a: str, hex_b: str, time: float) -> str:
     """Interpolate between two hex colors.
 
@@ -28,7 +29,8 @@ def _hex_interp(hex_a: str, hex_b: str, time: float) -> str:
     r, g, b = np.round(rgb_a + time * (rgb_b - rgb_a))
     return rgb_to_hex((r, g, b))
 
-_BG_COLOR = "#ac58fc"
+
+# _BG_COLOR = "#ac58fc"  # Sebastian's purple
 _BG_COLOR = "#b266b2"
 _PINSTRIPE_SCALE = 0.015
 _STROKE_SCALE = 0.01
@@ -37,52 +39,23 @@ _STROKE_COLOR = _hex_interp(_BG_COLOR, "#ffffff", 0.4)
 
 _COLOR_STEPS = 5
 
+# ==============================================================================
+#
+# re-use these time sequences (lux progressions) for any image
+#
+# ==============================================================================
 
 # re-use these for any image
-times = list(np.linspace(0, 1, _COLOR_STEPS))
-time_sequences = [dist.interpolate_floats(times, times, 1)]
+times = list(np.linspace(0, 1, _COLOR_STEPS, endpoint=False))
+time_sequences = [dist.linear(times)]
+
 for s in np.linspace(0.25, 1, 3):
-    time_sequences.append(dist.q1_time(times, s))
-    time_sequences.append(dist.q3_time(times, s))
-    time_sequences.append(dist.cos_time(times, s))
-    time_sequences.append(dist.sin_time(times, s))
-for p in np.linspace(1.25, 2, 3):
-    time_sequences.append(dist.pow_time(times, p))
-    time_sequences.append(dist.pow_time(times, 1 / p))
+    time_sequences.append(dist.push_f(times, s))
+    time_sequences.append(dist.push_l(times, s))
+    time_sequences.append(dist.push_i(times, s))
+    time_sequences.append(dist.push_o(times, s))
 
 
-
-
-def linear_interpolation(
-    input: Union[Path, str],
-    output: Union[Path, str],
-    num_layers: int,
-    black: str = "#000000",
-    white: str = "#ffffff",
-    stroke_shade: str = "#ff0000",
-) -> None:
-    """A default posterized effect.
-
-    :param input: The input png file.
-    :param output: The output svg file.
-    :param num_layers: The number of layers to use.
-    :param black: The color to use for the darkest layer.
-    :param white: The color to use for the lightest layer.
-    :param stroke_shade: The color to mix with the background to create the outline.
-
-    Will use a medium color for the background.
-    """
-    power = 1
-    luxs = np.linspace(0, 0.8**power, num_layers)
-    luxs = luxs ** (1 / power)
-    background = _hex_interp(white, black, 0.5)
-
-    black = "#000000"
-    white = "#eeeeee"
-    cols = [_hex_interp(white, black, lux) for lux in luxs]
-    stroke = _hex_interp(background, stroke_shade, 0.5)
-    stroke_width = 5
-    posterize_with_outline(input, output, luxs, cols, background, stroke, stroke_width)
 
 
 def _get_output_name(input_: Union[Path, str], infix: str) -> Path:
@@ -96,9 +69,9 @@ def try_lots(input_: Union[Path, str]):
     image = Image.open(input_)
     pinstripe_width = image.height * _PINSTRIPE_SCALE
     stroke_width = image.height * _STROKE_SCALE
-    cols = (
-        [_hex_interp("#ffffff", "#000000", t) for t in np.linspace(0, 1, _COLOR_STEPS)]
-    )
+    cols = [
+        _hex_interp("#ffffff", "#000000", t) for t in np.linspace(0, 1, _COLOR_STEPS)
+    ]
     for i, luxs in enumerate(time_sequences):
         print(luxs)
         posterize_with_outline(
--- src/posterize/time_distortions.py
+++ src/posterize/time_distortions.py
@@ -3,103 +3,121 @@
 input: time values from 0 to 1
 output: time values from 0 to 1
 
+linear: return input
+push_f: push first
+push_l: push last
+push_i: push in
+push_o: push out
+
+push_f and push_l give symmetrical results
+
+    push_f => [0.00, 0.08, 0.29, 0.62, 1.00]
+    push_l => [0.00, 0.38, 0.71, 0.92, 1.00]
+
+    but they are not the inverse of each other.
+    I.e., push_f(push_l(xs)) != xs && push_l(push_f(xs)) != xs
+
+push_i and push_o give inverse results
+
+    push_o => [0.00, 0.08, 0.29, 0.62, 1.00]
+    push_i => [0.00, 0.25, 0.50, 0.75, 1.00]
+
+    push_i(push_o(xs)) == xs && push_o(push_i(xs)) == xs
+
 :author: Shay Hill
 :created: 2023-08-01
 """
 
-import math
-from typing import Iterable
+import itertools as it
+from typing import Iterable, Sequence
 
-Q1 = math.pi / 2
-Q2 = Q1 * 2
-Q3 = Q1 * 3
+import numpy as np
 
 
-def interpolate_floats(
-    times_a: Iterable[float], times_b: Iterable[float], time: float
+def _average_floats(
+    floats_a: Sequence[float], floats_b: Sequence[float], time: float
 ) -> list[float]:
     """Interpolate between a and b for each a and b in times_a and times_b.
 
-    :param times_a: The first set of times.
-    :param times_b: The second set of times.
-    :param time: The time to interpolate at. 0.0 returns times_a, 1.0 returns times_b.
-    :return: The interpolated times.
+    :param floats_a: first sequence of values
+    :param floats_b: second sequence of values
+        (presumably the same number of items as floats_a)
+    :param time: The time to interpolate at. 0.0 returns floats_a, 1.0 returns floats_b
+    :return: The interpolated floats
     """
-    interpolated = [a + time * (b - a) for a, b in zip(times_a, times_b)]
-    # kludged in case of floating point errors
-    interpolated = [min(1, max(0.0, t)) for t in interpolated]
-    scale = 1 - 1 / len(interpolated)
-    return [t * scale for t in interpolated]
+    min_val = min(it.chain(floats_a, floats_b))
+    max_val = max(it.chain(floats_a, floats_b))
+    interpolated = [a + (b - a) * time for a, b in zip(floats_a, floats_b)]
+    return list(np.clip(interpolated, min_val, max_val))
 
 
+def linear(times: Iterable[float]) -> list[float]:
+    """This is a no-op. It returns the input."""
+    return [t for t in times]
 
-def q1_time(times: Iterable[float], strength: float = 1) -> list[float]:
-    """Distort time values with a sine wave in quadrant 1.
+
+def push_f(times: Sequence[float], strength: float = 1) -> list[float]:
+    """push_first, crowd values toward 0
 
     :param times: The times to distort. Float values (0.0, 1.0)
     :param strength: The strength of the distortion. 1.0 (default) is full distortion.
     :return: The distorted times. Float values (0.0, 1.0)
 
-    Samples crowd toward 1.
+    push_f([0.00, 0.25, 0.50, 0.75, 1.00]) =>
+           [0.00, 0.08, 0.29, 0.62, 1.00]
+
+    Distort time values with a sin wave in quadrant 4.
     """
-    distorted = [math.sin(t * Q1) for t in times]
-    return interpolate_floats(times, distorted, strength)
+    q4 = np.interp(times, [0.0, 1.0], [np.pi * 1.5, np.pi * 2])
+    distorted = np.interp([np.sin(r) for r in q4], [-1, 0], [0, 1])
+    return _average_floats(times, list(distorted), strength)
 
 
-def q3_time(times: Iterable[float], strength: float = 1) -> list[float]:
-    """Distort time values with a cos wave in quadrant 3.
+def push_l(times: Sequence[float], strength: float = 1) -> list[float]:
+    """push_last, crowd values toward 1
 
     :param times: The times to distort. Float values (0.0, 1.0)
     :param strength: The strength of the distortion. 1.0 (default) is full distortion.
     :return: The distorted times. Float values (0.0, 1.0)
 
-    Samples crowd toward 0.
+    push_l([0.00, 0.25, 0.50, 0.75, 1.00]) =>
+           [0.00, 0.38, 0.71, 0.92, 1.00]
+
+    Distort time values with a sine wave in quadrant 1.
     """
-    distorted = [math.cos(Q2 + t * Q1) + 1 for t in times]
-    return interpolate_floats(times, distorted, strength)
+    q1 = np.interp(times, [0.0, 1.0], [0, np.pi * 0.5])
+    distorted = [np.sin(r) for r in q1]
+    return _average_floats(times, distorted, strength)
 
 
-def cos_time(times: Iterable[float], strength: float = 1) -> list[float]:
-    """Distort time values with a cosine wave over quadrants 3 and 4.
+def push_o(times: Sequence[float], strength: float = 1) -> list[float]:
+    """push_out, crowd values away from 0.5
 
     :param times: The times to distort. Float values (0.0, 1.0)
     :param strength: The strength of the distortion. 1.0 (default) is full distortion.
     :return: The distorted times. Float values (0.0, 1.0)
 
-    Samples crowd toward endpoints.
+    push_o([0.00, 0.25, 0.50, 0.75, 1.00]) =>
+           [0.00, 0.15, 0.50, 0.85, 1.00]
+
+    Distort time values with a cosine wave over quadrants 3 and 4.
     """
-    distorted = [(math.cos(Q2 + t * Q2) + 1) / 2 for t in times]
-    return interpolate_floats(times, distorted, strength)
+    q3q4 = np.interp(times, [0, 1], [np.pi, np.pi * 2])
+    distorted = np.interp([np.cos(r) for r in q3q4], [-1, 1], [0, 1])
+    return _average_floats(times, list(distorted), strength)
 
 
-def sin_time(times: Iterable[float], strength: float = 1) -> list[float]:
-    """Distort time values with a sine wave over quadrants 1 and 3.
+def push_i(times: Sequence[float], strength: float = 1) -> list[float]:
+    """push_in, crowd values towards 0.5
 
-    :param times: The times to distort. Float values (0.0, 1.0)
-    :param strength: The strength of the distortion. 1.0 (default) is full distortion.
-    :return: The distorted times. Float values (0.0, 1.0)
+    :param times: The distorted times to reverse.
+    :param strength: The strength of the distortion. Should be the same as used in push_o function.
+    :return: The original times before distortion.
 
-    Samples crowd toward 0.5.
+    push_i([0.00, 0.25, 0.50, 0.75, 1.00]) =>
+           [0.00, 0.33, 0.50, 0.67, 1.00]
+
+    This is the inverse of push_o, so push_i(push_o(xs)) == xs
     """
-    distorted: list[float] = []
-    for time in times:
-        if time < 0.5:
-            distorted.append(math.sin(time * Q2) / 2)
-        else:
-            distorted.append((math.sin(Q3 + (time - 0.5) * Q2)) / 2 + 1)
-    return interpolate_floats(times, distorted, strength)
-
-
-def pow_time(times: Iterable[float], power: float = 1) -> list[float]:
-    """Distort times with a power function.
-
-    :param times: The times to distort. Float values (0.0, 1.0)
-    :param power: The strength of the distortion. 1.0 (default) is no distortion.
-        Values > 1 crowd toward 1.0. Values < 1 crowd toward 0.0. Don't use negative
-        numbers, because 0**neg will fail.
-    :return: The distorted times. Float values (0.0, 1.0)
-
-    Samples crowd toward 1.
-    """
-    distorted = [time**power for time in times]
-    return interpolate_floats(times, distorted, 1)
+    distorted = [np.arccos(x) / np.pi for x in np.interp(times, [0, 1], [1, -1])]
+    return _average_floats(times, distorted, strength)
