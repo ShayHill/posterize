e4ed8e1f8c12e443ab5764c27b2148e94bd74f86
Shay Hill <shay_public@hotmail.com>
2026-02-02 22:09:54 -0600

    refactor: simplify Posterization methods

--- src/posterize/posterization.py
+++ src/posterize/posterization.py
@@ -12,7 +12,7 @@ from __future__ import annotations
 
 import dataclasses
 from pathlib import Path
-from typing import TYPE_CHECKING, Annotated, TypeAlias, cast
+from typing import TYPE_CHECKING, Annotated, TypeAlias
 
 import numpy as np
 import svg_ultralight as su
@@ -24,32 +24,35 @@ from posterize.layers import merge_layers
 
 if TYPE_CHECKING:
     import os
-    from collections.abc import Iterable, Iterator
+    from collections.abc import Iterator
 
     from lxml.etree import (
         _Element as EtreeElement,  # pyright: ignore[reportPrivateUsage]
     )
 
-_IntA: TypeAlias = npt.NDArray[np.intp]
+# a (r, c) array of palette indices in (0,<= 512)
+Indices: TypeAlias = Annotated[npt.NDArray[np.intp], "(r, c)"]
+
+# a (n, 3) array of color vectors
+Palette: TypeAlias = Annotated[npt.NDArray[np.uint8], "(n, 3)"]
+
+# a (m, n) array. Each row contains a value for one color index. The values will be
+# either a single color index or -1 for transparent. Where a color index is present,
+# that color will replace colors on lower strate. -1 is transparent, allowing lower
+# strata to show through.
+Pstrata: TypeAlias = Annotated[npt.NDArray[np.intp], "(m, n)"]
+
+# a (n, r, m) array of layers, each layer with the same shape as the quantized image.
+# These are the strata expanded to the same shape as the quantized color indice.
+Layers: TypeAlias = Annotated[npt.NDArray[np.intp], "(n, r, c)"]
+
+# a (r, c, 3) array of RGB values
+Pixels: TypeAlias = Annotated[npt.NDArray[np.uint8], "(r, c, 3)"]
 
 
-def _expand_layers(
-    quantized_image: Annotated[npt.NDArray[np.intp], "(r, c)"],
-    d1_layers: Annotated[npt.NDArray[np.intp], "(n, 512)"],
-) -> Annotated[npt.NDArray[np.intp], "(n, r, c)"]:
-    """Expand layers to the size of the quantized image.
-
-    :param quantized_image: (r, c) array with palette indices
-    :param d1_layers: (n, 512) an array of layers. Layers may contain -1 or any
-        palette index in [0, 511].
-    :return: (n, r, c) array of layers, each layer with the same shape as the
-        quantized image.
-
-    Convert the (usually (512,)) layers of an ImageApproximation to the (n, r, c)
-    layers used for SVG rendering.
-    """
-    d1_layers_ = cast("Iterable[npt.NDArray[np.intp]]", d1_layers)
-    return np.array([x[quantized_image] for x in d1_layers_])
+def _get_layer_color(palette: Palette, layer: npt.NDArray[np.intp]) -> str:
+    color_index = next(x for x in layer.flatten() if x != -1)
+    return rgb_to_hex(palette[color_index])
 
 
 @dataclasses.dataclass
@@ -57,80 +60,62 @@ class Posterization:
     """Result of posterizing an image. Serializable.
 
     :param indices: (r, c) array with palette indices from the quantized image
-    :param palette: (512, 3) array of color vectors
-    :param layers: (n, 512) array of n layers, each containing a value (color index)
-        and -1 for transparent. Expanded to (n, r, c) internally for rendering.
+    :param palette: (n, 3) array of color vectors
+    :param pstrata: (m, n) array of n sequences, each containing one color index and
+        -1 for transparent. For each stratum, the one color index will appear over lower
+        colors it will replace.
     """
 
-    # fields are lists for json serialization
-    indices: list[list[int]]
-    palette: list[list[int]]
-    layers: list[list[int]]
+    indices: Annotated[npt.NDArray[np.intp], "(r, c)"]
+    palette: Annotated[npt.NDArray[np.uint8], "(n, 3)"]
+    pstrata: Annotated[npt.NDArray[np.intp], "(m, n)"]
 
     def __init__(
         self,
-        indices: Annotated[npt.NDArray[np.intp], "(r, c)"] | list[list[int]],
-        palette: Annotated[npt.NDArray[np.uint8], "(512, 3)"] | list[list[int]],
-        layers: Annotated[npt.NDArray[np.intp], "(n, 512)"] | list[list[int]],
+        indices: Annotated[npt.NDArray[np.intp], "(r, c)"] | npt.ArrayLike,
+        palette: Annotated[npt.NDArray[np.uint8], "(n, 3)"] | npt.ArrayLike,
+        pstrata: Annotated[npt.NDArray[np.intp], "(m, n)"] | npt.ArrayLike,
     ) -> None:
         """Initialize the Posterization.
 
         :param indices: (r, c) array with palette indices from the quantized image
         :param palette: (512, 3) array of color vectors
-        :param layers: (n, 512) array of n layers, each containing a value (color index)
-            and -1 for transparent
-
-        __init__ will only be passed numpy arrays in this library. The list types are to
-        restore a serialized Posterization object.
+        :param pstrata: (n, 512) array of n layers, each containing a value (color
+            index) and -1 for transparent
         """
-        indices = np.asarray(indices, dtype=np.intp)
-        palette = np.asarray(palette, dtype=np.uint8)
-        layers = np.asarray(layers, dtype=np.intp)
-        self.indices = indices.tolist()
-        self.palette = palette.tolist()
-        self.layers = layers.tolist()
+        self.indices = np.asarray(indices, dtype=np.intp)
+        self.palette = np.asarray(palette, dtype=np.uint8)
+        self.pstrata = np.asarray(pstrata, dtype=np.intp)
 
-        self._color_indices = tuple([next(x for x in y if x != -1) for y in layers])
-        self._expanded_layers = _expand_layers(indices, layers)
-        self._colors = [rgb_to_hex(palette[x]) for x in self._color_indices]
-        self.bbox = su.BoundingBox(0, 0, indices.shape[1], indices.shape[0])
-        self._svgds = [layer_to_svgd(x) for x in self._expanded_layers]
-
-    def get_layers(self, num_cols: int | None = None) -> list[_IntA]:
-        """Get the layers for the given number of colors."""
-        return list(self._expanded_layers[:num_cols])
-
-    def get_pixels(self, num_cols: int | None = None) -> _IntA:
-        """Get the color index for each pixel."""
-        return merge_layers(*self._expanded_layers[:num_cols])
-
-    def get_colors(self, num_cols: int | None = None) -> list[str]:
-        """Get the color for each layer."""
-        return self._colors[:num_cols]
-
-    def get_counts(self, num_cols: int | None = None) -> list[int]:
-        """Get the number of pixels for each color."""
-        bincount = np.bincount(self.get_pixels(num_cols).flatten())
-        return [int(bincount[x]) for x in self._color_indices[:num_cols]]
-
-    def get_svgds(self, num_cols: int | None = None) -> list[str]:
-        """Get the SVG data strings for each layer."""
-        return self._svgds[:num_cols]
-
-    def new_elements(self, num_cols: int | None = None) -> Iterator[EtreeElement]:
-        """Create new SVG path elements for each layer."""
-        colors = self.get_colors(num_cols)
-        svgds = self.get_svgds(num_cols)
-        for color, svgd in zip(colors, svgds, strict=True):
+        self.bbox = su.BoundingBox(0, 0, self.indices.shape[1], self.indices.shape[0])
+        self.layers = [self.pstrata[0][self.indices]]
+        self.svgds = [layer_to_svgd(self.layers[0])]
+        for stratum in self.pstrata[1:]:
+            layer = stratum[self.indices]
+            svgd = layer_to_svgd(layer)
             if not svgd:
                 continue
+            self.layers.append(layer)
+            self.svgds.append(svgd)
+        self.full_pixels = self.palette[self.indices]
+        self.part_pixels = merge_layers(*self.layers)
+        self.colors = [_get_layer_color(self.palette, x) for x in self.layers]
+        self.counts = [int(np.sum(layer != -1)) for layer in self.layers]
+
+    def new_elements(self, num: int | None = None) -> Iterator[EtreeElement]:
+        """Create new SVG path elements for each layer.
+
+        :param num: number of layers to create (default is all)
+        :yield: SVG path elements
+        """
+        for color, svgd in zip(self.colors[:num], self.svgds[:num], strict=True):
             yield su.new_element("path", d=svgd, fill=color)
 
-    def new_elem(self, num_cols: int | None = None) -> EtreeElement:
+    def new_elem(self, num: int | None = None) -> EtreeElement:
         """Create a new group element with the given number of layers."""
         tmat = f"matrix(1 0 0 -1 0 {self.bbox.height})"
         elem = su.new_element("g", transform=tmat)
-        for path in self.new_elements(num_cols):
+        for path in self.new_elements(num):
             elem.append(path)
         return elem
 
@@ -143,14 +128,12 @@ class Posterization:
         """Get the SVG root element."""
         return su.new_svg_root_around_bounds(self.new_blem(num_cols))
 
-    def write_svg(
-        self, path: str | os.PathLike[str], num_cols: int | None = None
-    ) -> Path:
+    def write_svg(self, path: str | os.PathLike[str], num: int | None = None) -> Path:
         """Write the posterization to an SVG file.
 
         :param path: path to the output SVG file
-        :param num_cols: optionally create the SVG with only the first `num_cols` colors
+        :param num: optionally create the SVG with only the first `num` colors
         :return: path to the output SVG file
         """
-        root = self.new_root(num_cols)
+        root = self.new_root(num)
         return su.write_svg(Path(path), root)
